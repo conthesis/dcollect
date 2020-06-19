04dda0d68c64636b83ddaf8f02140b61c3749ff1
whn <me@whn.se>
2020-06-19 02:40:39 +0200

    use nats for notifying

--- dcollect/notify.py
+++ dcollect/notify.py
@@ -1,4 +1,5 @@
 import asyncio
+from nats.aio.client import Client as NATS
 import logging
 import os
 
@@ -6,13 +7,13 @@ import httpx
 
 from dcollect.model import Model
 
-NOTIFY_TOPIC = "dcollect-notify-v1"
+NOTIFY_TOPIC = "entity-updates-v1"
 
 logger = logging.getLogger("dcollect.notify")
 
 
 class Notify:
-    http_client: httpx.AsyncClient
+    nc: NATS
     model: Model
     run: bool
     fut_done: asyncio.Future
@@ -21,6 +22,7 @@ class Notify:
         self.model = model
         self.http_client = http_client
         self.run = True
+        self.nc = NATS()
         loop = asyncio.get_event_loop()
         self.fut_done = loop.create_future()
 
@@ -28,12 +30,14 @@ class Notify:
         if os.environ.get("NO_SUBSCRIBE") == "1":
             self.fut_done.set_result(True)
             return
+        await self.nc.connect("nats://nats:4222")
         self.notify_task = asyncio.create_task(self.notify_loop())
         logging.info("Setup done")
 
     async def shutdown(self):
         self.run = False
         try:
+            await self.nc.drain()
             await asyncio.wait_for(self.fut_done, timeout=7.0)
         except asyncio.TimeoutError:
             self.notify_task.cancel()
@@ -44,17 +48,13 @@ class Notify:
             while self.run:
                 futures = []
                 async for notification in self.model.get_notifications():
-                    futures.append(asyncio.create_task(self.send_notification(
-                        "http://entwatcher:8000/v1/updates", notification.entity
-                    )))
+                    futures.append(asyncio.create_task(self.send_notification(notification.entity)))
                 asyncio.gather(*futures)
 
             self.fut_done.set_result(True)
         except asyncio.CancelledError:
             return
 
-    async def send_notification(self, url: str, entity: str) -> bool:
-        body = {"entity": entity}
-        resp = await self.http_client.post(url=url, json=body)
-        logger.info(f"Sent notification {url=} {resp.status_code=}")
-        return resp.status_code == 200
+    async def send_notification(self, entity: str) -> bool:
+        response = await self.nc.request(NOTIFY_TOPIC, entity.encode("utf-8"), timeout=5)
+        return response == b"OK"
